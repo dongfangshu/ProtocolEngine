868712f244761fe8ddb1ac2a13f05a9da067061c
dongfangshu <781477415@qq.com>
2022-12-01 23:19:27 +0800

    注释

--- ProtocolEngine/Gen.cs
+++ ProtocolEngine/Gen.cs
@@ -1,12 +1,130 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+//using System.Collections.Generic;
+//using System.Numerics;
 
-namespace ProtocolEngine
-{
-    internal class Gen
-    {
-    }
-}
+//namespace HeroData
+//{
+
+
+//    public class data1
+//    {
+//        public string str1;
+//        public bool b1;
+//        public int i1;
+//        public int i2;
+//        public data2 d2;
+//        public data3 d3;
+//        public list<data4> d4;
+//        public data5 d5;
+//        public data7 d7;
+//        public list<data8> d8;
+//        public int i3;
+//        public bool b2;
+//    }
+//    public class data2
+//    {
+//        public int i1;
+//        public int i2;
+//        public int i3;
+//        public data9 d9;
+
+//        public int i4;
+//        public int i5;
+//        public int i6;
+//        public int i7;
+//        public int i8;
+
+//        //升级的所有属性加成
+//        public int i9;
+//        public int i10;
+//        public int i11;
+//        public int i12;
+//        public int i13;
+//    }
+//    public class data4
+//    {
+//        public string str1; // id
+//        public string str2; // 配置表id
+//        public bool b1; // 锁定状态
+//        public int i1; // 等级
+//        public string str3; // 角色id
+//    }
+//    public class data3
+//    {
+//        public vector3 v1;
+//        public vector3 v2;
+//        public vector3 v3;
+//        public vector3 v4;
+//        public vector3 v5;
+
+//        public int i1;
+//        public int i2;
+//        public int i3;
+//        public int i4;
+//        public int i5;
+//    }
+//    public class data5
+//    {
+//        public string str1;
+//        public string str2;
+//        public bool b1;
+//        public int i1;
+//        public int i2;
+//        public int i3;
+//        public string str3;
+
+//        public int i4;
+//        public bool b2;
+//        public long l1;
+//        public int i5;
+//        public int i6;
+//        public data6 d6;
+
+//    }
+//    public class data6
+//    {
+//        public string s1;
+//        public string s2;
+//        public string s3;
+//        public string s4;
+//        public string s5;
+//        public string s6;
+
+//    }
+//    public class data7
+//    {
+//        public string str1;
+//        public list<string> list1;
+//        public list<string> list2;
+//        public list<string> list3;
+//        public list<string> list4;
+//        public list<string> list5;
+//    }
+//    public class data8
+//    {
+//        public int i1;
+//        public int i2;
+//        public float f1;
+//        public float f2;
+//    }
+//    public class data9
+//    {
+//        public datatype1 e1;
+//        public string s1;
+//        public string s2;
+//        public long l1;
+//    }
+//    public enum datatype1
+//    {
+//        t1 = 0,
+//        t2 = 1,
+//        t3 = 2,
+//        t4 = 3,
+//        t5 = 4,
+//        t6 = 5,
+//        t7 = 6,
+//        t8 = 7,
+//    }
+
+
+//}
